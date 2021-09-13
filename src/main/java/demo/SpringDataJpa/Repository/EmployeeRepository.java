5e1d3c8ce4a3612e699c8509d63dcb0f40b2d3fd
catluynh <catluynh99@gmail.com>
2021-09-13 20:12:53 +0700

    update native query

--- src/main/java/demo/SpringDataJpa/Repository/EmployeeRepository.java
+++ src/main/java/demo/SpringDataJpa/Repository/EmployeeRepository.java
@@ -2,7 +2,10 @@ package demo.SpringDataJpa.Repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,16 +13,22 @@ import demo.SpringDataJpa.Entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	
-	@Query(value ="select * from employee", nativeQuery = true)
+
+	@Query(value = "select * from employee", nativeQuery = true)
 	List<Employee> getAll();
-	
-	@Query(value ="insert into employee(first_name, last_name, email) values(?1, ?2, ?3)", nativeQuery = true)
+
+	@Transactional
+	@Modifying
+	@Query(value = "insert into employee(first_name, last_name, email) values(?1, ?2, ?3)", nativeQuery = true)
 	void addEmployee(String first_name, String last_name, String email);
-	
-	@Query(value ="update employee set first_name=?1, last_name=?2, email=?3 where id = ?4", nativeQuery = true)
+
+	@Transactional
+	@Modifying
+	@Query(value = "update employee set first_name=?1, last_name=?2, email=?3 where id = ?4", nativeQuery = true)
 	void updateEmployee(String first_name, String last_name, String email, Integer id);
-	
-	@Query(value ="delete from employee where id=?1", nativeQuery = true)
+
+	@Transactional
+	@Modifying
+	@Query(value = "delete from employee where id=?1", nativeQuery = true)
 	void deleteEmployee(int id);
 }
